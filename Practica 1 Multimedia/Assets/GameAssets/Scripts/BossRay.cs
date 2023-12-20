426663450d3653d91758d1dedf04a7810a23c386
luisep92 <luis@escolano.es>
2023-12-20 18:14:59 +0100

    hgd

--- Practica 1 Multimedia/Assets/GameAssets/Scripts/BossRay.cs
+++ Practica 1 Multimedia/Assets/GameAssets/Scripts/BossRay.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class BossRay : MonoBehaviour
 {
-    float speed = 30f;
-    SpriteRenderer sren;
-    float t;
+    private float speed = 30f;
+    private float t;
+    private SpriteRenderer sren;
+    private PolygonCollider2D col;
 
     private void Start()
     {
         sren = GetComponent<SpriteRenderer>();
+        col = GetComponent<PolygonCollider2D>();
     }
 
 
@@ -18,10 +20,36 @@ public class BossRay : MonoBehaviour
     {
         t += Time.deltaTime;
         if (t > 0.4f)
-            transform.Translate(- transform.up * Time.deltaTime * speed, Space.World);
+        {
+            float mod = Time.deltaTime * 3f;
+            transform.Translate(-transform.up * Time.deltaTime * speed, Space.World);
+            sren.size = new Vector2(sren.size.x - mod, sren.size.y);
+            // Adjust collider
+            Vector2 p0 = new(col.points[0].x + (mod/2), col.points[0].y);
+            Vector2 p1 = new(col.points[1].x + (mod/2), col.points[1].y);
+            Vector2 p2 = new(col.points[2].x - (mod/2), col.points[2].y);
+            Vector2 p3 = new(col.points[3].x - (mod/2), col.points[3].y);
+            Vector2[] points = { p0, p1, p2, p3 };
+            col.SetPath(0, points);
+        }
         else
-            sren.size = new Vector2(sren.size.x, sren.size.y + speed * Time.deltaTime);
+        {
+            float mod = speed * Time.deltaTime;
+            sren.size = new Vector2(sren.size.x, sren.size.y + mod);
+            // Adjust collider
+            Vector2 p1 = new(col.points[1].x, col.points[1].y - mod);
+            Vector2 p2 = new(col.points[2].x, col.points[2].y - mod);
+            Vector2[] points = { col.points[0], p1, p2, col.points[3] };
+            col.SetPath(0, points);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Player.Instance.GetDamage(2);
+    }
+
 
     private void OnBecameInvisible()
     {
